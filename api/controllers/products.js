83444464330ac054d7f541470cee2333f5ce4c19
NickMak <nikos@centaur.ag>
2019-09-06 22:48:08 +0300

    Fixed the products

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -3,16 +3,17 @@ const Product = require("../models/product");
 
 exports.products_get_all = (req, res, next) => {
   Product.find()
-    .select("name price _id productImage")
+    .select("_id title genre descr year")
     .exec()
     .then(docs => {
       const response = {
         count: docs.length,
         products: docs.map(doc => {
           return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
+            title: doc.title,
+            genre: doc.genre,
+            descr: doc.descr,
+            year: doc.year,
             _id: doc._id,
             request: {
               type: "GET",
@@ -40,9 +41,10 @@ exports.products_get_all = (req, res, next) => {
 exports.products_create_product = (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path
+    title: req.body.title,
+    genre: req.body.genre,
+    descr: req.body.descr,
+    year: req.body.year,
   });
   product
     .save()
@@ -51,8 +53,10 @@ exports.products_create_product = (req, res, next) => {
       res.status(201).json({
         message: "Created product successfully",
         createdProduct: {
-          name: result.name,
-          price: result.price,
+          title: result.title,
+          genre: result.genre,
+          descr: result.descr,
+          year: result.year,
           _id: result._id,
           request: {
             type: "GET",
@@ -72,7 +76,7 @@ exports.products_create_product = (req, res, next) => {
 exports.products_get_product = (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-    .select("name price _id productImage")
+    .select("_id title genre descr year")
     .exec()
     .then(doc => {
       console.log("From database", doc);
--- api/models/product.js
+++ api/models/product.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    productImage: { type: String, required: true }
+    title: { type: String, required: true },
+    genre: { type: String, required: true },
+    descr: { type: String, required: true },
+    year: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
